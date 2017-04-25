cf3002c65525d814bb4e92abe845f5410b93fc70
Andis Cirulis <andis.cirulis@whitedigital.eu>
2017-04-25 12:16:04 +0300

    default ubuntu seems still using version 2 docker compose format

--- webroot/index.php
+++ webroot/index.php
@@ -1,6 +1,22 @@
-<title>heh</title>
+<title>Docker test WD</title>
 <h1>Hello world from dockerized nginx</h1>
 
 <?php
+$dsn = 'mysql:host=db;dbname=dazadi';
+$username = 'root';
+$password = 'toor';
+$options = array(
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+); 
 
+$dbh = new PDO($dsn, $username, $password, $options);
+
+
+foreach ($dbh->query('select * from varda_dienas where month = 4') as $row) {
+        print $row['month'] . "\t";
+        print $row['day'] . "\t";
+        print $row['names'] . "\n";
+    }
+
+echo '------------';
 phpinfo();
