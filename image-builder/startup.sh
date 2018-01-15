681b320ae8243ae3b27994b06e9bcc517a9abb58
Andis Cirulis <andis.cirulis@whitedigital.eu>
2018-01-15 17:51:13 +0200

    PHP 7.1 support

--- image-builder/startup.sh
+++ image-builder/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/7.0/fpm/php.ini
-service php7.0-fpm start
+echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/7.1/fpm/php.ini
+service php7.1-fpm start
 nginx -g "daemon off;"
