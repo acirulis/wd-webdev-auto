265cbdaf30d9b56e85468d923908c56aaa0b7707
Andis Cirulis <andis.cirulis@whitedigital.eu>
2018-03-08 10:52:11 +0200

    php 5.6 & mysql 5.6

--- image-builder/startup.sh
+++ image-builder/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/7.2/fpm/php.ini
-service php7.2-fpm start
+echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/5.6/fpm/php.ini
+service php5.6-fpm start
 nginx -g "daemon off;"
