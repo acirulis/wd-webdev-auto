ed2397918a97396885d04250c90a4bef24fa2414
Andis Cirulis <andis.cirulis@whitedigital.eu>
2018-04-16 22:58:31 +0300

    initial apache/php5.6 setup

--- image-builder/startup.sh
+++ image-builder/startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/7.2/fpm/php.ini
-service php7.2-fpm start
-nginx -g "daemon off;"
+echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/5.6/apache2/php.ini
+
+# Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid
+
+/usr/sbin/apache2ctl -D FOREGROUND
