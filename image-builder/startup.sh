da127b51555acc187faa02bacdea77ca4f8945db
Andis Cirulis <andis.cirulis@whitedigital.eu>
2018-05-24 13:11:35 +0300

    reconfigure xdebug with callback

--- image-builder/startup.sh
+++ image-builder/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/5.6/apache2/php.ini
+# echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/5.6/apache2/php.ini
 
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
