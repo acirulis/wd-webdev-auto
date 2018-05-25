d067630dfcf27566b080dcf9b5c03f6c5729acd2
Andis Cirulis <andis.cirulis@whitedigital.eu>
2018-05-25 21:21:03 +0300

    ssl by default & xdebug

--- image-builder/startup.sh
+++ image-builder/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/5.6/fpm/php.ini
+# echo 'xdebug.remote_host='$XDEBUG_REMOTE_HOST >> /etc/php/5.6/fpm/php.ini
 service php5.6-fpm start
 nginx -g "daemon off;"
